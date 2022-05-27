16b6d56c6afeb5f28a953f18a74d9b7cd5f08e78
rianblockserena <ryannblock95@gmail.com>
2022-05-27 15:47:13 -0300

    code base t3

--- thread.h
+++ thread.h
@@ -1,18 +1,35 @@
 #ifndef thread_h
 #define thread_h
 
-#include <iostream>
 #include "cpu.h"
 #include "traits.h"
 #include "debug.h"
+#include "list.h"
+#include <ctime> 
+#include <chrono>
 
 __BEGIN_API
+
 class Thread
 {
 protected:
     typedef CPU::Context Context;
-
 public:
+
+    typedef Ordered_List<Thread> Ready_Queue;
+
+    // Thread State
+    enum State {
+        RUNNING,
+        READY,
+        FINISHING
+    };
+
+    /*
+     * Construtor vazio. Necessário para inicialização, mas sem importância para a execução das Threads.
+     */ 
+    Thread() { }
+
     /*
      * Cria uma Thread passando um ponteiro para a função a ser executada
      * e os parâmetros passados para a função, que podem variar.
@@ -20,17 +37,12 @@ public:
      * PS: devido ao template, este método deve ser implementado neste mesmo arquivo .h
      */ 
     template<typename ... Tn>
-    Thread(void (* entry)(Tn ...), Tn ... an){ 
-        _id = Thread::thread_counter++;
-        db<Thread>(INF) << "Thread criada com id:"<<_id;
-        _context = new Context( entry,an...);
-    }
+    Thread(void (* entry)(Tn ...), Tn ... an);
 
     /*
      * Retorna a Thread que está em execução.
      */ 
-    static Thread * running() {
-        return _running; }
+    static Thread * running() { return _running; }
 
     /*
      * Método para trocar o contexto entre duas thread, a anterior (prev)
@@ -46,17 +58,38 @@ public:
      * Quando a thread encerra, o controle deve retornar à main. 
      */  
     void thread_exit (int exit_code);
-        /*
-        da um switch com o main
-        e ai retorna ext_code
-        */
-        
-  
 
     /*
      * Retorna o ID da thread.
-     */
+     */ 
     int id();
+
+    /*
+     * NOVO MÉTODO DESTE TRABALHO.
+     * Daspachante (disptacher) de threads. 
+     * Executa enquanto houverem threads do usuário.
+     * Chama o escalonador para definir a próxima tarefa a ser executada.
+     */
+    static void dispatcher(); 
+
+    /*
+     * NOVO MÉTODO DESTE TRABALHO.
+     * Realiza a inicialização da class Thread.
+     * Cria as Threads main e dispatcher.
+     */ 
+    static void init(void (*main)(void *));
+
+
+    /*
+     * Devolve o processador para a thread dispatcher que irá escolher outra thread pronta
+     * para ser executada.
+     */
+    static void yield(); 
+
+    /*
+     * Destrutor de uma thread. Realiza todo os procedimentos para manter a consistência da classe.
+     */ 
+    ~Thread();
 
     /*
      * Qualquer outro método que você achar necessário para a solução.
@@ -69,12 +102,25 @@ private:
     int _id;
     Context * volatile _context;
     static Thread * _running;
-    static int thread_counter;
+    
+    static Thread _main; 
+    static CPU::Context _main_context;
+    static Thread _dispatcher;
+    static Ready_Queue _ready;
+    Ready_Queue::Element _link;
+    volatile State _state;
 
     /*
      * Qualquer outro atributo que você achar necessário para a solução.
      */ 
+
 };
+
+template<typename ... Tn>
+inline Thread::Thread(void (* entry)(Tn ...), Tn ... an) //: tem um inicializador/* inicialização de _link */
+{
+    //IMPLEMENTAÇÃO DO CONSTRUTOR
+}
 
 __END_API
 
